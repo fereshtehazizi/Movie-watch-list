a62182a194f0e3767993ecef556b36ade9863b5d
Fereshteh Azizi <fereshtehazizi710@gmail.com>
2026-02-11 23:00:40 +0430

    Refactor Summery component to display stats

--- src/components/Summery.jsx
+++ src/components/Summery.jsx
@@ -1,13 +1,21 @@
 export default function Summery({totalMovies, watchedCount, unwatchedCount}){
     return(
         <section className="summery">
-            <h1 className="headSubtitle">Summery</h1>
-            {unwatchedCount === 0 && totalMovies > 0 &&(
-            <p>You have watched everything!</p>
-        )}
-        <p>Total movies: {totalMovies}</p>
-        <p>Watched movies: {watchedCount}</p>
-        <p>Unwatched movies: {unwatchedCount}</p>
+            <h1 className="headSubtitle">Stats</h1>
+        <div className="stats">
+        <div className="statsTotal">
+            <span>{totalMovies}</span>
+            <span>Total Movies</span>
+        </div>
+        <div className="statsWatched">
+            <span>{watchedCount}</span>
+            <span>Have Watched</span>
+        </div>
+        <div className="statsUnwatched">
+            <span>{unwatchedCount}</span>
+            <span>To Watch</span>
+        </div>
+        </div>
         
         </section>
     );
